ad9aebaf0a3770686f9d7ef0d58381b2c8e044f0
Gregory Kofman <kofmangregory@gmail.com>
2020-05-30 19:13:34 -0400

    Created util to intialize and access MongoDriver client

--- leaselot-server/src/main/java/App.java
+++ leaselot-server/src/main/java/App.java
@@ -1,8 +1,16 @@
+import Config.MongoConfig;
+import com.mongodb.client.MongoDatabase;
 import io.javalin.Javalin;
+
+import java.util.Objects;
 
 public class App {
 
   public static void main(String[] args) {
+    MongoDatabase db =
+        MongoConfig.getMongoClient()
+            .getDatabase(Objects.requireNonNull(System.getenv("MONGO_DB_NAME")));
+
     Javalin app =
         Javalin.create(
                 config -> {
