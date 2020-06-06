abe238e12dce39d9d182dcd0167a730fc9e4ae4f
Gregory Kofman <kofmangregory@gmail.com>
2020-06-06 13:32:57 -0400

    Cleanup for sessions

--- leaselot-server/src/main/java/App.java
+++ leaselot-server/src/main/java/App.java
@@ -1,16 +1,8 @@
-import Config.MongoConfig;
-import com.mongodb.client.MongoDatabase;
 import io.javalin.Javalin;
-
-import java.util.Objects;
 
 public class App {
 
   public static void main(String[] args) {
-    MongoDatabase db =
-        MongoConfig.getMongoClient()
-            .getDatabase(Objects.requireNonNull(System.getenv("MONGO_DB_NAME")));
-
     Javalin app =
         Javalin.create(
                 config -> {
