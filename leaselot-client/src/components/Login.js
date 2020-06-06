f649d8327389a0fbf2180afacd4f663a5fb0dfcd
Gregory Kofman <kofmangregory@gmail.com>
2020-06-06 11:22:32 -0400

    Login page styled

--- leaselot-client/src/components/Login.js
+++ leaselot-client/src/components/Login.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import '../style/login.css';
 
 class Login extends React.Component {
 	render() {
 		return (
-			<div>
+			<div class="login-page">
+			  <img id="logo" src={require("../style/images/logo_leaselot.png")}/>
+			  <div class="form">
+			    <form class="login-form">
+			      <input type="text" placeholder="username"/>
+			      <input type="password" placeholder="password"/>
+			      <button><a id="button-link" href="dashboard">login</a></button>
+			    </form>
+			  </div>
 			</div>
 		);
 	}
