eb1be7614ed69e92fbe1fa7cf4a0d3eddfb67d65
Gregory Kofman <kofmangregory@gmail.com>
2020-06-20 15:43:33 -0400

    Added messages to dealership response

--- leaselot-server/src/main/java/App.java
+++ leaselot-server/src/main/java/App.java
@@ -1,15 +1,20 @@
+import Config.MongoConfig;
 import Config.SessionConfig;
+import Dealership.DealershipController;
+import com.mongodb.client.MongoDatabase;
 import io.javalin.Javalin;
+
+import java.util.Objects;
 
 public class App {
 
   public static void main(String[] args) {
-    //    MongoDatabase db =
-    //        MongoConfig.getMongoClient()
-    //            .getDatabase(Objects.requireNonNull(System.getenv("MONGO_DB_NAME")));
-    //
-    //    /** ******************** CONTROLLERS *************************** */
-    //    DealershipController dealershipController = new DealershipController(db);
+    MongoDatabase db =
+        MongoConfig.getMongoClient()
+            .getDatabase(Objects.requireNonNull(System.getenv("MONGO_DB_NAME")));
+
+    /** ******************** CONTROLLERS *************************** */
+    DealershipController dealershipController = new DealershipController(db);
 
     Javalin app =
         Javalin.create(
@@ -34,6 +39,6 @@ public class App {
             .start(Integer.parseInt(System.getenv("SERVER_PORT")));
 
     /** *************** Dealership Controller Routes *************** */
-    //    app.post("/enroll-dealership", dealershipController.enrollDealership);
+    app.post("/enroll-dealership", dealershipController.enrollDealership);
   }
 }
--- leaselot-server/src/main/java/Dealership/DealershipResponse.java
+++ leaselot-server/src/main/java/Dealership/DealershipResponse.java
@@ -3,20 +3,33 @@ package Dealership;
 import org.json.JSONObject;
 
 public enum DealershipResponse {
-  HASH_FAILURE("HASH_FAILURE"),
-  DUPLICATE_DEALERSHIP("DUPLICATE_DEALERSHIP"),
-  DUPLICATE_DEALER("DUPLICATE_DEALER"),
-  SUCCESS("SUCCESS");
+  HASH_FAILURE("HASH_FAILURE", "There was an error hashing the password."),
+  DUPLICATE_DEALERSHIP("DUPLICATE_DEALERSHIP", "The provided dealership name already exists."),
+  DUPLICATE_DEALER("DUPLICATE_DEALER", "The provided dealer username already exists."),
+  SUCCESS("SUCCESS", "Success.");
 
   private String status;
+  private String message;
 
-  DealershipResponse(String status) {
+  DealershipResponse(String status, String message) {
     this.status = status;
+    this.message = message;
+  }
+
+  public DealershipResponse setStatus(String status) {
+    this.status = status;
+    return this;
+  }
+
+  public DealershipResponse setMessage(String message) {
+    this.message = message;
+    return this;
   }
 
   public String toJSON() {
     JSONObject json = new JSONObject();
     json.put("status", this.status);
+    json.put("message", this.message);
     return json.toString();
   }
 }
